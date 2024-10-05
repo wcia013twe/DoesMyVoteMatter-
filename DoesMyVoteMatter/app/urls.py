abd761ba6916b8a992cb5523a80b2393a8ccf5cb
wcia013twe <wcia013twe@example.com>
2024-10-05 18:13:26 -0400

    completed the calculations

--- DoesMyVoteMatter/app/urls.py
+++ DoesMyVoteMatter/app/urls.py
@@ -3,6 +3,6 @@ from . import views
 from .models import District
 
 urlpatterns = [
-    path('home/', views.find_district, name='find_district'),
+    path('', views.find_district, name='find_district'),
     path('district-details/<str:state_abbreviation>/<int:district_number>/', views.district_details, name='district_details'),
 ]
