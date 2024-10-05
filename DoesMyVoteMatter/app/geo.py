15de91214d40778a12a83970c90e65633230892e
wcia013twe <wcia013twe@example.com>
2024-10-04 23:34:10 -0400

    csv works

--- DoesMyVoteMatter/app/geo.py
+++ DoesMyVoteMatter/app/geo.py
@@ -1,22 +1,28 @@
 import geopandas as gpd
 import fiona
 import pyogrio
+import csv
 
-relative_path = "../shapefiles/shp"
+relative_path = "../datasets/shp"
 gpd.read_file(relative_path)
 
-# layers = fiona.listlayers(relative_path)
+layers = fiona.listlayers(relative_path)
 
-layers = gpd.list_layers(relative_path)
+# layers = gpd.list_layers(relative_path)
 print(layers)
 
-for index, row in layers.iterrows():
-    print(f"District {index + 1}:")
-    print(f"Name: {row['Rucho_Lewis_Congress_3']}")
-    print(f"Geometry Type: {row['geometry'].geom_type}")
-    print(f"Coordinates: {row['geometry']}")
-    print()
+# for index, row in layers.iterrows():
+#     print(f"District {index + 1}:")
+#     print(f"Name: {row['Rucho_Lewis_Congress_3']}")
+#     print(f"Geometry Type: {row['geometry'].geom_type}")
+#     print(f"Coordinates: {row['geometry']}")
+#     print()
 
+with open('../datasets/csv/NCGA_Congress_2022_Court_-27833598801343649.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        print(row['District'], row['Party'], row['Congress'], row['Shape__Area'], row['Shape__Length'])
+        
 """
 proads.zip
 │── roads.shp       # Geometry data
