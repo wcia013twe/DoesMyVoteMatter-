5df41fadf0775bc28c8f11f1a39f86abb6b2442c
Ehsan Ayoubi <Ehsanayoubi2005@gmail.com>
2024-10-04 23:09:03 -0400

    added eg function

---
+++ DoesMyVoteMatter/app/math.py
@@ -0,0 +1,18 @@
+import math
+
+def calculate_efficiencygap(dem, rep):
+    # Add the two parameters
+    total = dem + rep
+    # Calculate the threshold by halving the total
+    threshold = math.ceil(total / 2)
+        
+    if dem > rep:
+        dem_waste = dem - threshold
+        rep_waste = rep
+        eg = (rep_waste - dem_waste) / total
+    else:
+        dem_waste = dem
+        rep_waste = rep - threshold
+        eg = (dem_waste - rep_waste) / total
+        
+    return round(eg, 10)
